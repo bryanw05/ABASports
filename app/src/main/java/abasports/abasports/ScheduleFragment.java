a92a0a5da62ff11d9b52fe4a7616a8e8b3185aa4
Bryan <bryanw05@hotmail.com>
2016-05-27 01:25:22 -0700

    changed a few things

--- app/src/main/java/abasports/abasports/ScheduleFragment.java
+++ app/src/main/java/abasports/abasports/ScheduleFragment.java
@@ -7,13 +7,24 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ScheduleFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.schedule_layout, null);
-    }
+
+       View rootView = inflater.inflate(R.layout.schedule_layout, null);
+
+        TextView dateTable = (TextView) rootView.getRootView().findViewById(R.id.date);
+        dateTable.setTextSize(20);
+        SimpleDateFormat df = new SimpleDateFormat("EEEE, MMMM d");
+        Date today = new Date();
+        dateTable.setText(df.format(today));
+
+        return rootView;
+    }//end onCreateView()
 
 }//end ScheduleFragment()
