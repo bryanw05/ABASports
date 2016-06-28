21e169ddb9a4b7e34fd127263b3845b53cfb0bb3
Bryan <bryanw05@hotmail.com>
2016-06-28 10:31:57 -0700

    updates

--- app/src/main/java/abasports/abasports/LoginFragment.java
+++ app/src/main/java/abasports/abasports/LoginFragment.java
@@ -5,9 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class LoginFragment extends Fragment{
+
+    private EditText username, password;
+    private Button login;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.login_layout,null);
