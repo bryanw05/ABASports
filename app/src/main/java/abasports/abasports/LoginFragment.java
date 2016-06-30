ef221f742c071d9511bf5afac6ed66b3909ca571
Bryan Wong <bryanw05@hotmail.com>
2016-06-30 16:36:34 -0700

    testing out with changes

--- app/src/main/java/abasports/abasports/LoginFragment.java
+++ app/src/main/java/abasports/abasports/LoginFragment.java
@@ -1,6 +1,7 @@
 package abasports.abasports;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +15,13 @@ public class LoginFragment extends Fragment{
     private EditText username, password;
     private Button login;
 
-
+    @Nullable
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.login_layout,null);
+        View rootView = inflater.inflate(R.layout.login_layout, null);
 
 
+        return rootView;
     }
 
 
