853ac9caf825179d297046b6015ce34c45f84045
Bryan Wong <bryanw05@hotmail.com>
2016-06-30 16:24:35 -0700

    testing out with changes

--- app/src/main/java/abasports/abasports/LoginFragment.java
+++ app/src/main/java/abasports/abasports/LoginFragment.java
@@ -17,6 +17,8 @@ public class LoginFragment extends Fragment{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.login_layout,null);
+
+
     }
 
 
