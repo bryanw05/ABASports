307ff0fc9f1819686364fd6a715c0a4e79fc3553
Bryan Wong <bryanw05@hotmail.com>
2016-06-30 18:14:55 -0700

    made a few changes

--- app/src/main/java/abasports/abasports/LoginFragment.java
+++ app/src/main/java/abasports/abasports/LoginFragment.java
@@ -20,9 +20,20 @@ public class LoginFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.login_layout, null);
 
+        username = (EditText) rootView.findViewById(R.id.username);
+        password = (EditText) rootView.findViewById(R.id.password);
+        login = (Button)rootView.findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+            }
+        });
 
         return rootView;
-    }
+    }//end onCreateView()
 
 
 }//end class LoginFragment
