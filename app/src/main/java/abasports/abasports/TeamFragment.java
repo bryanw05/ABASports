1c47104fbbc710ab28df1c45f68bac55652201fa
Bryan <bryanw05@hotmail.com>
2017-06-07 23:38:22 -0700

    updated files

---
+++ app/src/main/java/abasports/abasports/TeamFragment.java
@@ -0,0 +1,101 @@
+package abasports.abasports;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.ViewGroup.LayoutParams;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import models.ABAInterface;
+import models.Team;
+import models.TeamInfo;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class TeamFragment extends Fragment {
+
+    private String URL = "http://www.abasports.org/android/";
+    TableLayout team_table;
+    TeamInfo t = TeamInfo.getInstance();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.teams_layout, null);
+
+        team_table = (TableLayout)rootView.findViewById(R.id.teams);
+        team_table.setStretchAllColumns(true);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ABAInterface abaInterface = retrofit.create(ABAInterface.class);
+
+        Call<ArrayList<Team>> call = abaInterface.getStringResult();
+        call.enqueue(new Callback<ArrayList<Team>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Team>> call, Response<ArrayList<Team>> response) {
+                try {
+                    ArrayList<Team> TeamName = response.body();
+                        for (int i = 0; i < TeamName.size(); i++) {
+                            t.addTeam(TeamName.get(i));
+                        }
+
+                    final ArrayList<Team> teams = t.getData();
+                    Collections.sort(teams, new SortByName());
+                        for (int i = 0; i < teams.size(); i++) {
+                            TableRow row = new TableRow(getActivity());
+                            row.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+                            row.setId(i);
+                            int color = (i % 2 != 0) ? 0xff0000ff : 0xff00008B;
+                            row.setBackgroundColor(color);
+                            final String team_name = teams.get(i).getTeamName();
+                            TextView team_info = new TextView(getActivity());
+                            team_info.setId(i);
+                            team_info.setText(team_name);
+                            team_info.setTextColor(Color.parseColor("#FFFFFF"));
+                            team_info.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent profile = new Intent(getActivity(), TeamProfileActivity.class);
+                                    profile.putExtra("teamName", team_name);
+                                    startActivity(profile);
+                                }
+                            });
+                            row.addView(team_info);
+                            team_table.addView(row, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+                        }
+                } catch (Exception e){
+                    Log.d("onResponse", "There is an error");
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(Call<ArrayList<Team>> call, Throwable t) {
+                Log.d("onFailure", t.toString());
+            }
+        });
+        return rootView;
+    }
+    class SortByName implements Comparator<Team> {
+        public int compare(Team A, Team B) {
+            return A.getTeamName().compareTo(B.getTeamName());
+        }
+    }
+
+}
---
+++ app/src/main/java/models/Team.java
@@ -0,0 +1,135 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Team {
+
+        private String name;
+        private ArrayList<EmptyPlayer> players;
+        private String division;
+        private String streak;
+        private int wins;
+        private int losses;
+        private double winPercentage;
+
+        public Team(){}
+
+        public Team(String teamname){
+            this.name = teamname;
+            wins = 0;
+            losses = 0;
+            winPercentage = 0;
+        }
+
+        public Team(String team_Name, String division, int games_won, int games_lost, String current_streak) {
+            name = team_Name;
+            this.division = division;
+            wins = games_won;
+            losses = games_lost;
+            streak = current_streak;
+            winPercentage = calcPercentage();
+        }
+
+        public Team(String teamname, ArrayList<EmptyPlayer> p){
+            this.name = teamname;
+            players = p;
+            wins = 0;
+            losses = 0;
+            winPercentage = calcPercentage();
+        }
+
+        public ArrayList<EmptyPlayer> getPlayers(){
+            return players;
+        }
+
+        public void setPlayers(ArrayList<EmptyPlayer> players){
+            this.players = players;
+        }
+
+        public String getTeamName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public String getDivision(){
+            return division;
+        }
+
+        public void setDivision(String division){
+            this.division = division;
+        }
+
+        public int getWins(){
+            return wins;
+        }
+
+        public void setWins(int wins){
+            this.wins = wins;
+        }
+
+        public int getLosses(){
+            return losses;
+        }
+
+        public void setLosses(int losses){
+            this.losses = losses;
+        }
+
+        public double getWinPercentage(){
+            return calcPercentage();
+        }
+
+        public void setWinPercentage(double winPercentage){
+            this.winPercentage = winPercentage;
+        }
+
+        public String getRecord(){
+            return wins + "-" + losses;
+        }
+
+        public String getStreak() {
+            return streak;
+        }
+
+        public void setStreak(String streak) {
+            this.streak = streak;
+        }
+
+        public String showRoster(){
+            String roster="";
+            for (int i = 0; i < players.size(); i++){
+                roster += players.get(i).fullname()+ "\n";
+            }
+            return roster;
+        }
+
+        public double calcPercentage(){
+            if(wins == 0)
+                return 0;
+            return (1.0*wins/(wins+losses));
+        }
+
+        public void addEmptyPlayers(EmptyPlayer P){
+            players.add(P);
+        }
+
+        public EmptyPlayer removeEmptyPlayer(EmptyPlayer P){
+            int index = 0;
+            if(players.contains(P)){
+                System.out.println("Player found");
+                index = players.indexOf(P);
+                return players.remove(index);
+            } else{
+                System.out.println("Player not found");
+                return null;
+            }
+        }
+
+        public String toString(){
+            return name + "\n" + "record:" + wins + " - " + losses + "\n" + showRoster();
+        }
+}
+
